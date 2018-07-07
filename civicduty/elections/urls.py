b901042c9981638f7df7df2b3155b91a415a9230
warwithinabreth <newageideals@gmail.com>
2018-07-07 11:32:02 -0700

    Able to pull google civic api to django data base

---
+++ civicduty/elections/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+#from elections import views
+#from rest_framework.urlpatterns import format_suffix_patterns
+
+#urlpatterns = [
+    #path('',views.index, name='index'),
+    #path('dates/', views.ElectionList.as_view())
+#]
+
+#urlpatterns = format_suffix_patterns(urlpatterns)
