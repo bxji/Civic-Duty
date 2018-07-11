19317f9c2e72993f194892f99e3d40352e5174ca
Rob Phillips <rogphill@ucsc.edu>
2018-07-11 16:56:13 -0700

    Massively cleaned up repo

--- civicduty/elections/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Elections
-
-class  ElectionsSerializers(serializers.ModelSerializer):
-
-    class Meta:
-        model = Elections
-        fields = '__all__'
--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class Politician(models.Model):
-    name = models.charField(max_length=200)
-    party = models.TextField()
-    photo = models.ImageField(
-        upload_to="politicians/photos/", null=True, blank=True)
