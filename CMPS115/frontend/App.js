2f32d53d9ac94a83d2b06e077a99a13b1013fad5
Rob Phillips <rogphill@ucsc.edu>
2018-07-18 22:59:34 -0700

    House of Rep frameworks done, added into App.js

--- CMPS115/frontend/App.js
+++ CMPS115/frontend/App.js
@@ -1,267 +1,50 @@
-/* Must install the following denpendencies:
-* npm install --save react-navigation
-* npm install --save expo
-*/
-
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { Font } from 'expo';
-import { StackNavigator } from 'react-navigation';
-import VotingData from './assets/data/VotingData';
-
-<<<<<<< HEAD
-
-
-class MainPage extends React.Component {
-  static navigationOptions = {
-    headerStyle: {
-      backgroundColor: '#919793',
-    },
-  };
-
-  state = {
-    fontLoaded: false,
-  }
-
-  async componentWillMount(){
-    await Expo.Font.loadAsync({
-      'Georgia': require('./assets/fonts/GeorgiaBold.ttf'),
-      'Baskerville': require('./assets/fonts/Baskerville.ttf'),
-    });
-    this.setState({ fontLoaded: true });
-=======
-class App extends React.Component {
-  state = {
-    spinner: false
-  };
-
-  async handleFetch(url) {
-    try {
-      this.setState({ spinner: true });
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          zip: "92129"
-        })
-      });
-      const dataSource = response.json();
-      this.setState({ dataSource: result.officials, spinner: false });
-    } catch (e) {
-      this.setState({ spinner: false });
-      console.error(error);
+import {
+    StyleSheet,
+    Text,
+    View } from 'react-native';
+  import React from 'react';
+  import { createStackNavigator } from 'react-navigation'
+  
+  //screen import(s) for the splash screen
+  import MainPage from './screens/MainPage'
+  
+  //screen import(s) for links on the splash screen
+  import FindRepPage from './screens/FindRepPage'
+  //import RegisterToVotePage from './screens/RegisterToVotePage'
+  import InfoPage from './screens/InfoPage'
+  
+  //screen import(s) for links of the links on the splash screen
+  //import ListOfRepresentativePage from './screens/ListOfRepresentativePage'
+  import DemSenatorPage from './screens/DemSenatorPage'
+  //import RepubSenatorPage from './screens/RepubSenatorPage'
+  import DemHouseRepPage from './screens/DemHouseRepPage'
+  import RepubHouseRepPage from './screens/RepubHouseRepPage'
+  //import ZipCodeInputPage from './screens/ZipCodeInputPage'
+  
+  
+  //import VotingData from './assets/data/VotingData'
+  
+  //Main driver that starts the app. Calls AppNavigator
+  export default class App extends React.Component {
+    render() {
+      return (
+        <AppNavigator/>
+      );
     }
   }
-
-  componentDidMount() {
-    this.handleFetch("http://civic-duty.herokuapp.com/representative/");
->>>>>>> abd76a8bbea42fbe21763f4d3fb6073408bccdef
-  }
-
-  render() {
-    return (
-<<<<<<< HEAD
-      <View style={styles.container}>
-      {
-        this.state.fontLoaded ? (
-          <Text style={styles.textTitle}>
-          Civic Duty{"\n"}
-            <Text style={styles.textSubTitle}>Informing the Masses
-            </Text>
-          </Text>
-        ) : null
-      }
-      <Button
-        onPress={() => this.props.navigation.navigate('RepresentativePage')}
-        title="Find Your Rep"
-        color = '#919793'
-      />
-      </View>
-    );
-  }
-}
-
-class FindRepScreen extends React.Component {
-  static navigationOptions = {
-    headerStyle: {
-      backgroundColor: '#919793',
-    },
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Button
-        title="List of Reps"
-        onPress={() => this.props.navigation.push('ListOfRepsPage')}
-=======
-      <View style={{ flex: 1, paddingTop: 20 }}>
-        {this.state.spinner && <Spinner />}
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={({ item }) => (
-            <Text>
-              {item.name}, {item.party}, {/*{item.ocdDivisionId}*/}
-            </Text>
-          )}
-          keyExtractor={(item, index) => index.toString()}
->>>>>>> abd76a8bbea42fbe21763f4d3fb6073408bccdef
-        />
-      </View>
-    );
-  }
-<<<<<<< HEAD
-}
-
-class ListOfRepresentativesPage extends React.Component {
-  constructor(props) {
-    super(props)
-
-  }
-  static navigationOptions = {
-    headerStyle: {
-      backgroundColor: '#919793',
-    },
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-          <VotingData />
-      </View>
-    );
-  }
-}
-
-class DemSenatorPage extends React.Component {
-  constructor(props) {
-    super(props)
-
-  }
-  static navigationOptions = {
-    headerStyle: {
-      backgroundColor: '#2B60DE',
-    },
-  };
-
-  render() {
-    return (
-      <View style={styles.demBackGround}>
-        <VotingData />
-      </View>
-    );
-  }
-}
-
-class RepubSenatorPage extends React.Component {
-  state = {
-    fontLoaded: false,
-  }
-  static navigationOptions = {
-    headerStyle: {
-      backgroundColor: '#CC0000',
-    },
-  };
-
-  render() {
-    return (
-      <View style={styles.repubBackGround}>
-      </View>
-    );
-  }
-}
-
-class IndepSenatorPage extends React.Component {
-  state = {
-    fontLoaded: false,
-  }
-  static navigationOptions = {
-    headerStyle: {
-      backgroundColor: '#59459F',
-    },
-  };
-
-  render() {
-    return (
-      <View style={styles.indepBackGround}>
-      </View>
-    );
-  }
-}
-
-const RootStack = StackNavigator(
-  {
-    FrontPage: {
-      screen: MainPage,
-    },
-    RepresentativePage: {
-      screen: FindRepScreen,
-    },
-    ListOfRepsPage: {
-      screen: ListOfRepresentativesPage,
-    },
-    RepubPage: {
-      screen: RepubSenatorPage,
-    },
-    IndepPage: {
-      screen: IndepSenatorPage,
-    },
-  },
-  {
-    initialRouteName: 'FrontPage',
-  }
-);
-
-export default class App extends React.Component {
-  render() {
-    return <RootStack />;
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#919793',
-    alignItems: 'center',
-  },
-  textTitle: {
-    fontSize: 27,
-    fontFamily: 'Georgia',
-    color: '#FFFFFF',
-    marginTop: 70,
-    marginBottom: 80,
-  },
-  textSubTitle: {
-    fontSize: 13,
-    fontFamily: 'Baskerville',
-    color: '#FFFFFF',
-    alignItems: 'center',
-  },
-  textLinks: {
-    fontSize: 16,
-    color: '#FFFFFF',
-    alignItems: 'center',
-    marginLeft: 40,
-  },
-  demBackGround: {
-    flex: 1,
-    backgroundColor: '#2B60DE',
-    alignItems: 'center',
-  },
-  repubBackGround: {
-    flex: 1,
-    backgroundColor: '#CC0000',
-    alignItems: 'center',
-  },
-  indepBackGround: {
-    flex: 1,
-    backgroundColor: '#59459F',
-    alignItems: 'center',
-  },
-});
-=======
-}
->>>>>>> abd76a8bbea42fbe21763f4d3fb6073408bccdef
+  
+  
+  //creates a stack navigator. creates the ablility to navigate pages
+  const AppNavigator = createStackNavigator ({
+    MainPage : { screen: MainPage },
+    FindRepPage : { screen: FindRepPage },
+    //RegisterToVotePage : { screeen: RegisterToVotePage },
+    InfoPage : { screen: InfoPage },
+    //ListOfRepresentativePage: { screen: ListOfRepresentativePage},
+    DemSenatorPage : { screen: DemSenatorPage },
+    //RepubSenatorPage : { screen: RepubSenatorPage },
+    DemHouseRepPage : { screen: DemHouseRepPage },
+    RepubHouseRepPage : { screen: RepubHouseRepPage }
+    //ZipCodeInputPage : { screen: ZipCodeInputPage },
+  
+  })
